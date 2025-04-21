a465ac3326c6952cdbb805aa25e006089451c527
Iga <bucz1991@gmail.com>
2025-04-22 00:52:52 +0200

    feat: Dodanie poczatkowego pliku aplikacji z podstawową polityką CORS

---
+++ fitapp_api/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost", "http://localhost:8081", "http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    return {"message": "FitApp Backend"}
