9c45f9b96f4a188e955f614089cca0a677fdc07c
Marcin <marcin_krolak@op.pl>
2025-04-22 23:35:21 +0200

    feat: Dodanie funkcji do modułu trips

---
+++ fitapp_api/gps/utlis.py
@@ -0,0 +1,14 @@
+from fitapp_api.gps.models import GPSPoint
+from fitapp_api.gps.db import gps_db
+from asyncpg.exceptions._base import UnknownPostgresError
+
+# Funkcje pomocnicze
+async def get_session_ids_by_user_id(user_id: str) -> list[str]:
+    query = f"SELECT session_id from gps_points WHERE user_id ilike '{user_id}' GROUP BY session_id"
+    try:
+        async for connection in gps_db.get_connection():
+            results = await connection.fetch(query)
+        return [str(row[0]) for row in results]
+    except UnknownPostgresError as e:
+        return []
+
