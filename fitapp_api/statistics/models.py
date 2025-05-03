673035dacb61261161b453fb11d2f8e4954ca232
Iga <bucz1991@gmail.com>
2025-05-03 21:11:52 +0200

    fix: poprawka w opisie modeli statystyk, poprawka strefy czasowej w docker-compose

--- fitapp_api/statistics/models.py
+++ fitapp_api/statistics/models.py
@@ -12,16 +12,16 @@ class StatisticsRequest(BaseModel):
 
 class StatisticActivity(BaseModel):
 	activity: TripActivity = Field(description="Typ aktywności")
-	distance: float = Field(ge=0, description="Dystans w km")
-	time: float = Field(ge=0, le=10000, description="Czas w godzinach")
+	distance: float = Field(ge=0, description="Dystans w m")
+	time: float = Field(ge=0, description="Czas w sekundach")
 	calories_burned: float = Field(ge=0, description="Liczba spalonych kalorii")
 	count: int = Field(ge=0, le=10000, description="Liczba wystąpień aktywności")
 
 
 class StatisticsResponse(BaseModel):
-	user_id: int = Field(ge=1, le=1000000, description="ID użytkownika")
+	user_id: int = Field(ge=1, description="ID użytkownika")
 	average_speed: float = Field(ge=0, description="Średnia prędkość w km/h")
-	total_distance: float = Field(ge=0, description="Całkowity dystans w km")
+	total_distance: float = Field(ge=0, description="Całkowity dystans w m")
 	total_time: float = Field(ge=0, description="Całkowity czas w sekundach")
 	total_calories_burned: float = Field(ge=0, description="Całkowita liczba spalonych kalorii")
 	activities: list[StatisticActivity] = Field(description="Lista aktywności")
